d1a5c68c43f8804e43da364e3a59e748e4965b8c
HoangTN <HoangTN@DESKTOP-H68FGFI>
2021-09-27 01:02:04 +0700

    Initial commit

---
+++ src/adtractexample/AdtractExample.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adtractexample;
+
+/**
+ *
+ * @author HoangTN
+ */
+public class AdtractExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bike obj = new Honda4();
+        obj.run();
+    }
+
+}
---
+++ src/adtractexample/Honda4.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adtractexample;
+
+/**
+ *
+ * @author HoangTN
+ */
+public class Honda4 extends  Bike{
+        void run() {
+        System.out.println("running safely..");
+    }
+
+}
